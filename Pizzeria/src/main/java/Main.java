d515669ba11000562f9e6e1d7dbaa2859307fe6d
Tomek <Tomek@MamutPC>
2014-01-17 17:19:46 +0100

    Projekt

---
+++ Pizzeria/src/main/java/Main.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.example.db.HsqlUnitOfWork;
+import org.example.db.dao.ClientDao;
+import org.example.db.dao.HsqlClientDao;
+import org.example.db.dao.HsqlPizzaDao;
+import org.example.pizza.Client;
+import org.example.pizza.Pizza;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+			HsqlUnitOfWork uow = new HsqlUnitOfWork();
+			ClientDao dao = new HsqlClientDao(uow);
+			Client c = new Client();
+			
+			c.setEmail("a@wp.pl");
+			c.setName("Jan");
+			c.setSurname("Nowak");
+			c.setNumber("1234");
+			c.setId(0);
+			
+			Client c1 = new Client();
+			c1.setId(0);
+			dao.delete(c1);
+			dao.save(c);
+			uow.commit();
+			uow.close();
+		System.out.println("koniec");
+	}
+
+}
---
+++ Pizzeria/src/main/java/org/example/pizza/Order.java
@@ -0,0 +1,44 @@
+package org.example.pizza;
+
+import java.util.Date;
+import java.util.List;
+
+import org.example.db.EntityBase;
+
+public class Order extends EntityBase {
+	
+	private String number;
+	private Date date;
+	
+	
+	//relacja zamowienie - klient
+	private Client client;
+	
+	
+	
+	//relacja zamowienie - pizza
+	private List<Pizza> products;
+	
+	
+	
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	} 
+	
+	
+}
